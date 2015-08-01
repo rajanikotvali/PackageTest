2682410bfe02b9bf683b1286537c1b859440a10a
rajanikanth <kotvali.rajani@gmail.com>
2015-08-01 23:17:28 +0530

    added SOP statements

--- src/main/java/com/test/packex/PackageDemo.java
+++ src/main/java/com/test/packex/PackageDemo.java
@@ -3,8 +3,11 @@ package com.test.packex.packone;
 public class PackageDemo
 {
 	public static void main(String args[]){
+		System.out.println("In FirstClass object");
 		FirstClass fc = new FirstClass();
+		System.out.println("In SecondClass object");
 		SecondClass sc = new SecondClass();
+		System.out.println("In ThirdClass object");
 		ThirdClass tc = new ThirdClass();
 	}
 }
